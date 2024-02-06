c2542084409999cad3f27409648fd8daefb8bd5e
Priyanshi-Vadadoriya <priyanshivadadoriya888@gmail.com>
2024-02-06 11:10:02 +0530

    ...

--- ASSESSMENTS/myapp/src/App.js
+++ ASSESSMENTS/myapp/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
-import { useState } from 'react';
+import React, { useState, useContext } from 'react';
+
+
+const CheckboxContext = React.createContext();
 
 function App() {
-
+  
   const [checkedItems, setCheckedItems] = useState({});
 
   const handleChange = (event) => {
@@ -11,47 +13,56 @@ function App() {
       [event.target.name]: event.target.checked
     });
   };
-  return (
-    <div className="App">
-          <label>
-        <input
-          type="checkbox"
-          name="option1"
-          checked={checkedItems.option1 || false}
-          onChange={handleChange}
-        />
-        Option 1
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="option2"
-          checked={checkedItems.option2 || false}
-          onChange={handleChange}
-        />
-        Option 2
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          name="option3"
-          checked={checkedItems.option3 || false}
-          onChange={handleChange}
-        />
-        Option 3
-      </label>
-      <br />
 
-      <div>
-        Selected Options:
-        {Object.keys(checkedItems).map((key) => (
-          checkedItems[key] && <p key={key}>{key}</p>
-        ))}
+  return (
+    
+    <CheckboxContext.Provider value={{ checkedItems, handleChange }}>
+      <div className="App">
+        <label>
+          <input
+            type="checkbox"
+            name="option1"
+            checked={checkedItems.option1 || false}
+            onChange={handleChange}
+          />
+          Option 1
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="option2"
+            checked={checkedItems.option2 || false}
+            onChange={handleChange}
+          />
+          Option 2
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="option3"
+            checked={checkedItems.option3 || false}
+            onChange={handleChange}
+          />
+          Option 3
+        </label>
+        <br />
+
+        <div>
+          Selected Options:
+          {Object.keys(checkedItems).map((key) => (
+            checkedItems[key] && <p key={key}>{key}</p>
+          ))}
+        </div>
       </div>
-    </div>
+    </CheckboxContext.Provider>
   );
 }
+
+
+export const useCheckboxContext = () => {
+  return useContext(CheckboxContext);
+};
 
 export default App;
